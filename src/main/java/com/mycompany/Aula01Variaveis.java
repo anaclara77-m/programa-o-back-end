5b33ce2188459ab503a87c7cb2f71b4331260789
anaclara77-m <gorges.ana@escola.pr.gov.br>
2025-03-26 19:40:34 +0000

    utilizando print

--- src/main/java/com/mycompany/Aula01Variaveis.java
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -2,13 +2,19 @@ package com.mycompany;
 
 public class Aula01Variaveis {
     public static void main(String[] args) {
-    // Variável lógica
+        // Variável lógica
 
-    // Tipo primitívo
-    boolean valorlogicoprimitivo = true;
+        // Tipo primitívo
+        boolean valorLogicoPrimitivo = true;
 
-    // Tipo abstrato
-    boolean Valorlogicoabstrato = false; 
+        // Exibe o valor
+        System.out.println(valorLogicoPrimitivo);
+
+
+        // Tipo abstrato
+        boolean valorLogicoAbstrato = false; 
+        // Exibe com texto
+        System.out.println("O valor a variavel valorLogicoAbstrato é: " + valorLogicoAbstrato);
     
     } 
 }
