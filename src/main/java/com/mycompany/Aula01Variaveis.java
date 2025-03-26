80d36d7b4d09a8a944676dc23b10732e0ba3f221
anaclara77-m <gorges.ana@escola.pr.gov.br>
2025-03-26 13:51:35 +0000

    aula01.variaveis

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+    public static void main(String[] args) {
+    // Variável lógica
+
+    // Tipo primitívo
+    boolean valorlogicoprimitivo = true;
+
+    // Tipo abstrato
+    boolean Valorlogicoabstrato = false; 
+    
+    } 
+}
