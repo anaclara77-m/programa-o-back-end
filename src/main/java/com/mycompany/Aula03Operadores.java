d3a443f803698c81870376dd597f28b8895139ff
anaclara77-m <gorges.ana@escola.pr.gov.br>
2025-04-04 14:03:53 +0000

    aula 03 de operadores

--- src/main/java/com/mycompany/Aula03Operadores.java
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -14,7 +14,36 @@ public class Aula03Operadores {
           * linhas
           */
 
-          
+         /** 
+          * Declaracão de variáveis 
+          */
+
+          String var1; // Toda variável declarada sem valor, automaticamente é null,
+          String var2 = null; // Variável declarada com valor null
+          String var3 = ""; // Variável declarada com valor vazio (diferente de null)
+
+          /**
+           * Operadores Matemáticos
+           */
+
+           int num1 = 10;
+           int num2= 2;
+
+           // Soma
+           int soma = num1 + num2;
+           System.out.println("A soma entre " + num1 + " e " + num2 + " é: " + soma);
+
+           // Subtração
+           int subtração = num1 * num2;
+           System.out.println("A subtração de " + num1 + " por " + num2 + " é: " + subtração);
+
+           // Multiplicação
+           int multiplicação = num1 * num2;
+           System.out.println("A multiplicação de " + num1 + " por " + num2 + " é: " + multiplicação);
+
+           // Divisão 
+           int divisão = num1 * num2;
+           System.out.println("A divisão de " + num1 + " por " + num2 + " é: " + divisão);
         
          
         
