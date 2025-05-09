72e76f70026cff2aaa4cb39cfc9254cf3b1f32ec
anaclara77-m <gorges.ana@escola.pr.gov.br>
2025-05-09 13:19:55 +0000

    aula.05.Repeticoes

--- src/main/java/com/mycompany/Aula05Repeticao.java
+++ src/main/java/com/mycompany/Aula05Repeticao.java
@@ -31,10 +31,42 @@ public class Aula05Repeticao {
          *
          * É utilizada para ler valores do usuário inseridos pelo prompt.
          */
-        Scanner scan = new Scanner(System.in); // Cria o scanner
-        System.out.println("Digite um número: ");
+        // Scanner scan = new Scanner(System.in); // Cria o scanner
+        // System.out.println("Digite um número: ");
 
-        int numero = scan.nextInt(); // Lê o valor digitado e capturado pelo scanner
-        System.out.println("Você digitou " + numero + "!!");
+        // int numero = scan.nextInt(); // Lê o valor digitado e capturado pelo scanner
+        // System.out.println("Você digitou " + numero + "!!");
+
+        /**
+         * While (enquanto)
+         *
+         * Enquanto a condição for verdadeira o código será executado
+         * diferente da próxima estrutura, essa faz a validação antes da execução
+         */
+        int num1 = 1;
+
+        while (num1 != 0) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite um número: (0 para parar)");
+            num1 = scan.nextInt();
+
+            System.out.println("Você digitou " + num1 + "!!");
+        }
+
+        System.out.println("Execução encerrada");
+        
+        /**
+         * Do While (execute, enquanto)
+         * É executada uma estrutura de código e após é validada a condição
+         */
+        int num2 = 1;
+
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite um número: (0 para Sair)");
+            num2 = scan.nextInt();
+        } while (num2 != 0);
+
+        System.out.println("Execução encerrada");
     }
 }
