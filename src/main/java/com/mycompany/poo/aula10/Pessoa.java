18eb947221effd699ae3968f75a94d5f15da73d5
anaclara77-m <gorges.ana@escola.pr.gov.br>
2025-08-06 13:41:51 +0000

    Aula 10, modificadores de acesso , prt 2

---
+++ src/main/java/com/mycompany/poo/aula10/Pessoa.java
@@ -0,0 +1,47 @@
+package com.mycompany.poo.aula10;
+
+public class Pessoa {
+    /*
+     * Modificadores de acesso
+     * 
+     * Quando trabalhamos com o conceito de POO, utilizamos modificadores de acesso
+     * para dar segurança para nossas classes.
+     * 
+     * Utilizamos elas como prefixo para:
+     * -Classes
+     * -Interfaces
+     * -Atributos
+     * -Métodos
+     */
+    public String nome; // Público, é visivel em qualquer classe.
+    protected int idade; // Protegido, é visivel somente em classes filhas (Herança).
+    private double salario; // Privado, é visivel somente dentro da propria classe.
+    double altura; // Friendly ou Público, apesar de não estar explicitado, ele também é visivel.
+
+    public void exibirDados() {
+        System.out.println("--- Dados da pessoa ---");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura (m): " + altura);
+
+    }
+
+    /**
+     * Nomenclatura de métodos
+     * 
+     * Métodos/Funções indicam ações, e por padrão utilizamos verbos.
+     * 
+     * Ex. : correr, andar, caminhar ...
+     */
+    public void atribuirSalario(double valor) {
+        this.salario = valor;
+
+        System.out.println("O novo salario de " + this.nome + " é: " + this.salario);
+    }
+
+    public void atribuirSalario(Double valor, double bonus){
+        this.salario = valor + bonus;
+
+        print("O novo salário de " + this.nome + "é: " + this.salario + " mais um bonus de " + bonus);
+    }
+}
